693a50a898c849ed83dc8ab79d72a079ca050ab5
Tommy Long <tommy.long@autointegrate.com>
2021-01-22 00:27:16 +0000

    add missing tests (rider tripping balls)

---
+++ tests/Devbot.FluentTesting.Tests/StateTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace FluentGwt.Tests
+{
+    // ReSharper disable once UnusedType.Global
+    public class StateTests
+    {
+        [Fact]
+        public async Task CanExecuteState()
+        {
+            var executed = false;
+            var given = State.Given(() => executed = true);
+            await given.Execute();
+            executed.Should().BeTrue();
+        }
+    }
+}
